c28f5805266db4cc789bcfe6ce05b39e5f6f6953
Nathan Garcia <n8g423@gmail.com>
2023-09-14 16:00:02 -0600

    Assignment 3

--- app/page.js
+++ app/page.js
@@ -3,15 +3,17 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main class="flex min-h-screen flex-col items-center justify-between p-24">
-      <div class="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
-        <h1 class="text-4xl">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <h1 className="text-4xl">
           CPRG 306: Web Development 2 - Assignments
         </h1>
 
         <StudentInfo />
 
         <Link href="week2">Week 2</Link>
+        <br />
+        <Link href="week3">Week 3</Link>
       </div>
     </main>
   );
--- app/week3/page.js
+++ app/week3/page.js
@@ -1,15 +1,15 @@
-import StudentInfo from "../StudentInfo";
+import ItemList from "./item-list";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-34">
+    <main className="flex min-h-screen flex-col justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1 className="text-4xl">My Shopping List</h1>
-
-        <div>
-          {" "}
-          <StudentInfo />
-        </div>
+      </div>
+      <br />
+      <div className="items-left grid grid-cols-3">
+        {" "}
+        <ItemList />
       </div>
     </main>
   );
