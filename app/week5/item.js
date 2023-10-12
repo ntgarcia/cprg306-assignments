5927b1d11bf5edf3bb1f92f3c3f89cbe972da6d8
Nathan Garcia <n8g423@gmail.com>
2023-10-12 00:20:44 -0600

    Week5 fix

--- app/week5/item.js
+++ app/week5/item.js
@@ -1,6 +1,6 @@
 export default function Item({ name, quantity, category }) {
   return (
-    <div className="bg-neutral-950 rounded-xl p-5 m-5">
+    <div className="bg-neutral-950 rounded-xl p-8 m-5 space-y-3">
       <h3 className="text-xl font-bold">{name}</h3>
       <p>quantity: {quantity}</p>
       <p>category: {category}</p>
